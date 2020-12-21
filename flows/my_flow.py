a9787cfbde226d2734e77609704cfb2bbf0d0fac
pheadra <luke.park@toss.im>
2020-12-22 08:42:12 +0900

    first commit

--- flows/my_flow.py
+++ flows/my_flow.py
@@ -5,23 +5,24 @@ from prefect.executors.dask import DaskExecutor
 from prefect.storage import GitHub
 from prefect.run_configs import KubernetesRun
 
+
 @task
 def get_data():
     return [1, 2, 3, 4, 5]
+
 
 @task
 def print_data(data):
     print(data)
 
-with Flow("file-based-flow") as flow:
+
+with Flow("file-based-flow",
+          executor=DaskExecutor("tcp://dask-scheduler:8786"),
+          run_config=KubernetesRun(),
+          storage=GitHub(
+                  repo="pheadra/prefect",  # name of repo
+                  path="flows/my_flow.py",  # location of flow file in repo
+                  ref="main"
+          )) as flow:
     data = get_data()
     print_data(data)
-
-
-flow.run_config = KubernetesRun()
-flow.executor = DaskExecutor("tcp://dask-scheduler:8786")
-flow.storage = GitHub(
-    repo="pheadra/prefect",                 # name of repo
-    path="flows/my_flow.py",       # location of flow file in repo
-    ref="main"
-)
